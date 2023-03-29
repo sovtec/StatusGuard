fcfc28b038a1017d9c92e4907515d3dcf1fe503c
hrdu_ <ragnarbergseth@yahoo.no>
2023-03-30 00:25:23 +0200

    Edit: index & webhookURL

---
+++ src/webHookURL.js
@@ -0,0 +1,3 @@
+//Bruk din egen URL || Use your own webhook URL
+export const webhookURL =
+  "https://discord.com/api/webhooks/1090759721029476422/7EF9BLvwgEpCGClutlz_xnaeBbLyRvBGdT7qTkVYkLCa-S_aB2P6sUU0qgShYmW7ABD6";
